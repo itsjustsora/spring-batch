6a33ca219e22ed66d236af8330fa380f18811e2a
keemsora <itsyoursora@gmail.com>
2025-01-17 21:37:26 +0900

    feat: Tasklet 구현 및 배치 실행 코드 작성
    - Tasklet 구현
    - 설정 클래스에 배치 빈 등록
    - Job, Step 생성 및 빈 등록

---
+++ src/main/java/dev/lab/springbatch/configuration/BasicTaskJobConfiguration.java
@@ -0,0 +1,49 @@
+package dev.lab.springbatch.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import dev.lab.springbatch.jobs.GreetingTask;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class BasicTaskJobConfiguration {
+
+    private final PlatformTransactionManager transactionManager;
+    private final JobRepository jobRepository;
+
+    @Bean
+    public Tasklet greetingTasklet() {
+        return new GreetingTask();
+    }
+
+    @Bean
+    public Step step() {
+        log.info("---------------------- Init step01 ----------------------");
+
+        return new StepBuilder("step01", jobRepository)
+            .tasklet(greetingTasklet(), transactionManager)
+            .build();
+    }
+
+    @Bean
+    public Job job() {
+        log.info("---------------------- Init job01 ----------------------");
+
+        return new JobBuilder("job01", jobRepository)
+            .incrementer(new RunIdIncrementer())
+            .start(step())
+            .build();
+    }
+}
