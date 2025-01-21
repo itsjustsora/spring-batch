d1eba0a8790ac23d732481e9e290b0121c29ccfb
keemsora <itsyoursora@gmail.com>
2025-01-21 22:50:24 +0900

    feat: FlatFileItemReader, FlatFileItemWriter 간단 구현
    - csv 파일 추가

---
+++ src/main/java/dev/lab/springbatch/configuration/FileJobConfiguration.java
@@ -0,0 +1,84 @@
+package dev.lab.springbatch.configuration;
+
+import static javax.xml.transform.OutputKeys.*;
+
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import dev.lab.springbatch.domain.Movie;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class FileJobConfiguration {
+
+    public static final int CHUNK_SIZE = 100;
+    public static final String ENCODING = "UTF-8";
+
+    private final PlatformTransactionManager transactionManager;
+    private final JobRepository jobRepository;
+
+    @Bean
+    public Step flatFileStep() {
+        log.info("==================== Init flatFileStep ====================");
+
+        return new StepBuilder("flatFileStep", jobRepository)
+            .<Movie, Movie>chunk(CHUNK_SIZE, transactionManager)
+            .reader(flatFileItemReader())
+            .writer(flatFileItemWriter())
+            .build();
+    }
+
+    @Bean
+    public Job flatFileJob() {
+        log.info("==================== Init flatFileJob ====================");
+        return new JobBuilder("flatFileJob", jobRepository)
+            .incrementer(new RunIdIncrementer())
+            .start(flatFileStep())
+            .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<Movie> flatFileItemReader() {
+        return new FlatFileItemReaderBuilder<Movie>()
+            .name("flatFileItemReader")
+            .resource(new ClassPathResource("./movies.csv"))
+            .encoding(ENCODING)
+            .delimited().delimiter(",")
+            .names("title", "genre", "year")
+            .targetType(Movie.class)
+            .build();
+    }
+
+    @Bean
+    public FlatFileItemWriter<Movie> flatFileItemWriter() {
+        return new FlatFileItemWriterBuilder<Movie>()
+            .name("flatFileItemWriter")
+            .resource(new FileSystemResource("./output/movie_new.csv"))
+            .encoding(ENCODING)
+            .delimited().delimiter(",")
+            .names("title", "genre", "year")
+            .build();
+
+    }
+}
---
+++ src/main/java/dev/lab/springbatch/domain/Movie.java
@@ -0,0 +1,14 @@
+package dev.lab.springbatch.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Movie {
+
+    private String title;
+    private String genre;
+    private String year;
+
+}
